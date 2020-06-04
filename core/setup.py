fa486d0fdb78cc25ac97a6ecb7359174273221b4
basfora <beatriz.asfora@gmail.com>
2020-06-04 17:42:20 -0400

    folder structure

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup  # , find_packages
 
 setup(name='core',
       version='0.0.0',
-      url='https://github.coecis.cornell.edu/ba386/multiRobotTargetSearch',
+      url='https://github.com/basfora/milp_mespp.git',
       author='Beatriz Asfora',
       author_email='beatriz.asfora@gmail.com',
       packages=['core', ],
